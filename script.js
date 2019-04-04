0843170726e96bcc40fe963aa0ebadfba7c46720
kakoitomax <max36598@gmail.com>
2019-04-04 17:15:03 +0200

    Lesson1-JS

---
+++ script.js
@@ -0,0 +1,39 @@
+/* 1 Task */
+var secondsInMin = 60;
+var minInHour = 60;
+var hours = 3;
+
+console.log (secondsInMin * minInHour * hours)
+
+/* 2 Task */
+    var num = 1;
+    num +=12;
+    num-=14;
+    num *=5;
+    num /=7;
+    num ++;
+    num --;
+    alert (num);
+
+/* 3 Task */
+    var a = 10;
+    var b = 2;
+    var c = 5;
+    var result = a + b + c;
+
+    document.write (result);
+    document.write ("<br />");
+/* 4 Task */
+    var a = 17;
+    var b = 10;
+    var result_1 = a - b;
+    document.write (result_1);
+    document.write ("<br />");
+    var c = result_1;
+    var d = 7;
+    var result_2 = c - d;
+    document.write (result_2);
+    document.write ("<br />");
+/* 5 Task */
+    var name = "Max";
+    document.write (name = "Hello Max!");
